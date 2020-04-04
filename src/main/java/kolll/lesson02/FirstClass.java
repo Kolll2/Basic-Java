9d478104667e55f69aa5a5cabdd8194346ded7c9
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-04-04 22:43:51 +0400

    lesson 02 Class

---
+++ src/main/java/kolll/lesson02/FirstClass.java
@@ -0,0 +1,9 @@
+package kolll.lesson02;
+
+/**
+ * repeating classes and their interactions
+ */
+
+public class FirstClass {
+
+}
---
+++ src/main/java/kolll/lesson02/Main.java
@@ -0,0 +1,15 @@
+package kolll.lesson02;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        //create link FirstClass type, 32 bit (4 byte) on stack function Main
+        FirstClass firstClass = null;
+
+        //create in heap class FirstClass and 
+        firstClass = new FirstClass();
+    }
+
+}

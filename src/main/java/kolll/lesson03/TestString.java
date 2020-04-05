ccc7e9778e66da441a6bca99627c774fd50775df
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-04-05 19:55:38 +0400

    repeat lesson 03

---
+++ src/main/java/kolll/lesson03/TestString.java
@@ -0,0 +1,20 @@
+package kolll.lesson03;
+
+public class TestString {
+
+    public static void main(String[] args) {
+        String s = "Hello";
+        String t = new String(s);
+
+        System.out.println(s);
+        System.out.println(t);
+
+        if ("Hello".equals(s)) System.out.println("one");
+        if (t == s) System.out.println("two");
+        if (t.equals(s)) System.out.println("three");
+        if ("Hello" == s) System.out.println("four");
+        if ("Hello" == t) System.out.println("five");
+    }
+
+
+}

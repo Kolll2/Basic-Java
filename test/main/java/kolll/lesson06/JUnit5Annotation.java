6e216b5acf9092814e132cbf54a353e0aa5101eb
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-04-10 22:17:52 +0400

    Create JUnit5Annotation.java

---
+++ test/main/java/kolll/lesson06/JUnit5Annotation.java
@@ -0,0 +1,19 @@
+package main.java.kolll.lesson06;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class JUnit5Annotation {
+
+    /*
+    Эта аннотация означает, что метод является тестовым методом, не принимающий параметров.
+     */
+    @Test
+    void helloJUnit5(){
+        assertEquals(10, 5+5);
+    }
+
+}

63864162bc8ff9d698460a08ba5c80f98394e129
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-09-10 13:18:57 +0400

    Update Main.java

--- src/main/java/kolll/lesson11/Main.java
+++ src/main/java/kolll/lesson11/Main.java
@@ -4,12 +4,21 @@ package kolll.lesson11;
 
 // SimpleDateFormat
 
-
-
 import java.text.SimpleDateFormat;
+
+/**
+ * Look up test folder
+ */
 
 public class Main {
     public static void main(String[] args) {
+
+        SimpleDateFormatTest sdft = new SimpleDateFormatTest();
+
+        for (int i = 0; i < 30 ; i++) {
+            System.out.println(sdft.getRandomDate());
+        }
+
 
     }
 }

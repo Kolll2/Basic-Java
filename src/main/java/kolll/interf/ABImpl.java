9e16ffcc221824491b44084bfd88a1d4d2459960
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-09-10 13:18:33 +0400

    Update ABImpl.java

--- src/main/java/kolll/interf/ABImpl.java
+++ src/main/java/kolll/interf/ABImpl.java
@@ -16,26 +16,26 @@ public class ABImpl implements A, B {
         Object obj = new Object();
         Hashtable<Object, Integer> hashtable = new Hashtable<>();
 
-        nodeListTest();
+//        nodeListTest();
     }
 
-    private static void nodeListTest() {
-
-        NodeList nodeList1 = new NodeList(1);
-        NodeList nodeList2 = new NodeList(2);
-        NodeList nodeList3 = new NodeList(3);
-        NodeList nodeList4 = new NodeList(4);
-
-        System.out.println(nodeList1 + " " + nodeList2 + " " + nodeList3 + " " + nodeList4);
-
-        nodeList1.setNext(nodeList2); // to 2
-        nodeList2.setNext(nodeList3); // to 3
-        nodeList3.setNext(nodeList4); // to null
-
-
-        NodeList.reverse(nodeList1);
-        System.out.println(nodeList1 + " " + nodeList2 + " " + nodeList3 + " " + nodeList4);
-    }
+//    private static void nodeListTest() {
+//
+//        NodeList nodeList1 = new NodeList(1);
+//        NodeList nodeList2 = new NodeList(2);
+//        NodeList nodeList3 = new NodeList(3);
+//        NodeList nodeList4 = new NodeList(4);
+//
+//        System.out.println(nodeList1 + " " + nodeList2 + " " + nodeList3 + " " + nodeList4);
+//
+//        nodeList1.setNext(nodeList2); // to 2
+//        nodeList2.setNext(nodeList3); // to 3
+//        nodeList3.setNext(nodeList4); // to null
+//
+//
+//        NodeList.reverse(nodeList1);
+//        System.out.println(nodeList1 + " " + nodeList2 + " " + nodeList3 + " " + nodeList4);
+//    }
 
     @Override
     public int getInner() {
